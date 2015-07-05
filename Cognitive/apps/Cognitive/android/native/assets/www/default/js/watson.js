3fcf9125b480c0d8c0ba5600cd3bb4121c345bdc
Percival Lucena <percival@motorola.com>
2015-07-04 22:55:09 -0300

    first commit

---
+++ Cognitive/apps/Cognitive/android/native/assets/www/default/js/watson.js
@@ -0,0 +1,23 @@
+
+/* JavaScript content from js/watson.js in folder common */
+$("#searchbtn" ).click(function() {
+  searchvalue = $("#searchterm").val();	
+  
+  var gbWS ="http://pergunte.mybluemix.net/watson/search/"+searchvalue;
+	// faz chamda ao WS REST via ajax	
+	  $.ajax({
+		type: 'GET',
+		url: gbWS,
+		crossDomain: true,
+		dataType: "json", 
+		success: renderList2, // função de retorno do WS
+		error: function(xhr, status, error) {
+			  var err = eval("(" + xhr.responseText + ")");
+			  alert(err.Message);
+		}
+	});	  
+});
+
+function renderList2(data) {
+	alert(data);
+}
---
+++ Cognitive/apps/Cognitive/android/native/assets/www/default/worklight/checksum.js
@@ -0,0 +1,2 @@
+var WL_CHECKSUM = {"checksum":2882248651,"date":1436061188651,"machine":"R2D2.local"};
+/* Date: Sat Jul 04 22:53:08 BRT 2015 */
